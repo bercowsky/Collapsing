f923417dbfc76abbfe6612c8b24c8105fa99e2fd
Andrés Bercowsky Rama <andresbercowskyrama@MacBook-Pro-de-Andres.local>
2023-06-28 22:47:40 +0200

    Added main scripts and updated readme

--- README.md
+++ README.md
@@ -8,4 +8,30 @@ python3 setup.py build_ext --inplace
 ```
 
 ## Usage
-To run evaluation using STEGO as a feature extractor, run `eval_stego.py`. 
+To run evaluation using STEGO as a feature extractor, run `eval_stego.py`. This script will extract features from a user-defined number of images, then creates meta-points for them, and finally run Collapsing to assign pseudo-labels for each pixel. The script will then evaluate the performance of Collapsing using the pseudo-labels and the ground truth labels, logging the results into W&B.
+
+To run evaluation using DINO, DINOv2 or SAM as a feature extractor, run `eval.py`. As before, this script extracts features from a user-defined number of images using the selected model, then creates meta-points for them, and finally run Collapsing to assign pseudo-labels for each pixel. The script will then evaluate the performance of Collapsing using the pseudo-labels and the ground truth labels, logging the results into W&B.
+
+Each script has a number of options to select, including the number of images to use, the distance metric to use (between euclidean and 1-cosine_similarity), the radius of the meta-points and the `k` for the `k`-nearest neighbors algorithm used in Collapsing. Additionally, in `eval.py` the user can select the model to use as a feature extractor, and the option to apply PCA to those features.
+
+These scripts expect the following directory structure:
+
+```
+.
+├── PREFIX/
+│   ├── ADEChallengeData2016/
+│   │   ├── annotations/
+│   │   │   ├── training/
+│   │   │   │   ├── *.png
+│   │   │   ├── validation/
+│   │   │   │   ├── *.png
+│   │   ├── images/
+│   │   │   ├── training/
+│   │   │   │   ├── *.jpg
+│   │   │   ├── validation/
+│   │   │   │   ├── *.jpg
+│   ├── meta_points/
+│   ├── saved_models/
+```
+
+The `PREFIX` directory is the root directory of the dataset. The `ADEChallengeData2016` directory contains the ADE20K dataset. The `annotations` directory contains the ground truth labels for the training and validation sets. The `images` directory contains the images for the training and validation sets. The `meta_points` directory is an empty directory that will contain .png files where each pixel value represent the meta_point id representing that pixel. The `saved_models` directory contains the saved models for the feature extractors.
--- src/eval_stego.py
+++ src/eval_stego.py
@@ -1,17 +1,19 @@
-"""Evaluation script for the DINO, DINOv2 and SAM models.
+"""Evaluation script for the STEGO model.
 
-The script runs inference on the images of the ADE20K validation set and
+The script runs inference on the images of the ADE20K training set and
 computes the mIoU, homogeneity, completeness and V-measure scores, uploading
 the results to W&B. The results include images with the segmentation masks
 overlaid on top of the original images.
 
 The input to the script are the following:
-    * The model to be used for inference
     * The distance metric
     * The meta-points radius
     * The k for K-Nearest Neighbors
     * The number of images to process
-    * Whether to apply PCA or not (getting the first 90 principal components)
+
+The radius is computed using the distance metric provided. For the Cosine
+distance, the radius is 1 - cosine_similarity, so smaller values mean more
+meta-points created.
 
 This script assumes you follow the directory structure described bellow:
 
@@ -27,156 +29,137 @@ This script assumes you follow the directory structure described bellow:
                 - *.jpg
             - validation/
                 - *.jpg
-    - labels/
-        - collapsing/
-        - kmeans/
+    - meta_points/
     - saved_models/
-
 """
 
-
 import os
-import sys
-import time
-from tqdm import tqdm
-
-import numpy as np
+from os.path import join
+import wget
 from PIL import Image
-from segment_anything import sam_model_registry
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
+from tqdm import tqdm
+import time
+import sys
+
 import torch
 import torch.nn.functional as F
-from torchvision import transforms as T
+import numpy as np
+from sklearn.cluster import KMeans
 import wandb
-import matplotlib.pyplot as plt
 
-import cython.clustering
-from eval_utils import (
+import cython.clustering as clustering
+from utils.eval_utils import (
     CustomDataset,
     get_class_labels,
     map_pseudo_to_label,
-    measure_from_confusion_matrix,
+    measure_from_confusion_matrix
 )
-from cython.feature_extractor import get_dino_embeddings
 
-# Constants
-# TODO: Pass as an argument to the script
-BATCH_SIZE = 1
+sys.path.append("STEGO/src")
+
+from STEGO.src.train_segmentation import LitUnsupervisedSegmenter
+
 
 # TODO: Pass as an argument to the script
 PREFIX = '/Users/andresbercowskyrama/Desktop/UNI/TFG/Datasets/'
 
+# Constants
+BATCH_SIZE = 1
+NUM_GT_CLASSES = 150
+
 DATASET_DIR     = os.path.join(PREFIX, 'ADEChallengeData2016/')
-COLLAPSING_PATH = os.path.join(PREFIX, 'labels/collapsing')
-KMEANS_PATH     = os.path.join(PREFIX, 'labels/kmeans')
+REPS_DIR        = os.path.join(PREFIX, 'representatives/')
+METAPOINTS_PATH = os.path.join(PREFIX, 'meta_points/')
 LABEL_DIR       = os.path.join(DATASET_DIR, 'annotations/training')
-CHECKPOINT_PATH = os.path.join(PREFIX, 'saved_models', 'sam_vit_h_4b8939.pth')
-
-# Model
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-def get_principal_components(X, n):
-    """Apply PCA to the given data and return the first n principal components."""
-    pca = PCA(n_components=n)
-    pca.fit(X)
-    print("Explained variance:", np.sum(pca.explained_variance_ratio_))
-    return torch.from_numpy(pca.transform(X).astype(np.float32))
-    
-
-def process_batch(img, model, meta_points, config):
-    """Runs inference on a batch of images and return the features.
-
-    We run inference on the image and its horizontal flip and then average the results
-    to get a more robust representation.
 
 
-    """
-    shape = img.shape[-2:]
-    if config.model == 'SAM':
-        with torch.no_grad():
-            img = T.Resize((1024, 1024), antialias=True)(img)
-            code1 = model.image_encoder(img)
-            code2 = model.image_encoder(img.flip(dims=[3]))
-    elif config.model.startswith('DINO'):
-        with torch.no_grad():
-            img = T.Resize((1024, 1024), antialias=True)(img)
-            code1 = torch.tensor(get_dino_embeddings(DEVICE, model, img))
-            code2 = torch.tensor(get_dino_embeddings(DEVICE, model, img.flip(dims=[3])))
+# Download pretrained Model
+os.chdir("STEGO/src")
+saved_models_dir = join("..", "saved_models")
+os.makedirs(saved_models_dir, exist_ok=True)
 
-    code = (code1 + code2.flip(dims=[3])) / 2
-    # Output shape: (batch_size, dimensions, img_widht, img_height)
-    code = F.interpolate(code, shape, mode='bilinear', align_corners=False)
+saved_model_url_root = "https://marhamilresearch4.blob.core.windows.net/stego-public/saved_models/"
+saved_model_name = "cocostuff27_vit_base_5.ckpt"
+if not os.path.exists(join(saved_models_dir, saved_model_name)):
+    wget.download(saved_model_url_root + saved_model_name, join(saved_models_dir, saved_model_name))
 
-    # Apply PCA to the features
-    if config.use_pca:
-        code = get_principal_components(code[0].permute(1,2,0).reshape((-1, code.shape[1])), 90).reshape((1, shape[0], shape[1], 90)).permute(0, 3, 1, 2)
-    features = np.array(code[0].permute(1,2,0).reshape(-1, code.shape[1]))
+# Load pretrained STEGO
+model = LitUnsupervisedSegmenter.load_from_checkpoint(join(saved_models_dir, saved_model_name), map_location=torch.device('mps')).cpu()
 
-    # Create meta-points
-    meta_points, reps = clustering.create_meta_points(features, config.radius, config.metric)
 
+def process_batch(model, img, meta_points, config):
+    # Query model and pass results through CRF
+    with torch.no_grad():
+        code1 = model(img)
+        code2 = model(img.flip(dims=[3]))
+        code  = (code1 + code2.flip(dims=[3])) / 2
+        code = F.interpolate(code, img.shape[-2:], mode='bilinear', align_corners=False)
+
+    features = np.array(code[0].permute(1,2,0).reshape(-1, 90))
+    if len(meta_points) == 0:
+        meta_points, reps = clustering.create_meta_points(features, config.radius, config.metric)
+    else:
+        meta_points, reps = clustering.update_meta_points(meta_points, features, config.radius)
     return reps, meta_points
 
 
-def png_path_name(model_name, image_name):
-    """Returns the path to the png file where the labels will be saved."""
-    return os.path.join(COLLAPSING_PATH, model_name + image_name.split('.')[0] + '.png')
-    
-
-def inference(model, dataloader, config=None):
+def inference(config=None):
     config = wandb.config
 
-    target_clusters = 150
-
+    # Create the dataset
+    dataset = CustomDataset(DATASET_DIR)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
     meta_points = np.empty(0, dtype=object)
     current_meta_point = np.empty(0, dtype=object)
+    num_meta_points = []
+    times_meta_points = []
     for batch_idx, samples in tqdm(enumerate(dataloader), total=config.num_images):
         if batch_idx == config.num_images:
             break
-
-        reps, current_meta_point = process_batch(samples[0], model, current_meta_point, config)
-        # Shift the representative ids by the number of meta points already created
+        start_time = time.time()
+        reps, current_meta_point = process_batch(model, samples[0], current_meta_point, config)
         reps += len(meta_points)
+        total_time = time.time() - start_time
+        times_meta_points.append(total_time)
+        img = Image.fromarray(reps.astype(np.uint16))
+        img.save(os.path.join(METAPOINTS_PATH, samples[2][0].split('.')[0] + '.png'), bitdepth=16, format='PNG')
+        num_meta_points.append(len(current_meta_point) + len(meta_points))
 
-        img = Image.fromarray(reps.astype(np.uint32))
-        img.save(png_path_name(config.model, samples[2][0]), bitdepth=32, format='PNG')
-
-        meta_points = np.append(meta_points, current_meta_point)
-        current_meta_point = np.empty(0, dtype=object)
+        if total_time >= 1 or batch_idx == config.num_images - 1:
+            meta_points = np.append(meta_points, current_meta_point)
+            current_meta_point = np.empty(0, dtype=object)
 
     centers = np.array([mp.get_center() for mp in meta_points])
 
-    kmeans = KMeans(n_clusters=min(len(centers), target_clusters), n_init='auto', random_state=0).fit(centers)
+    kmeans = KMeans(n_clusters=min(len(centers), 150), random_state=0).fit(centers)
     labels_kmeans = kmeans.labels_
 
     # Collapse and label meta points
     start_time = time.time()
-    collapsed_meta_points = clustering.collapse_meta_points(meta_points, config.k_neighbors, config.metric)
+    collapsed_meta_points = clustering.collapse_meta_points(meta_points, config.k_neighbors)
     print("Collapsing took {} seconds".format(time.time() - start_time))
     start_time = time.time()
-    labels_meta_points = clustering.get_labels_meta_points(collapsed_meta_points, 0, config.metric)
+    labels_meta_points = clustering.get_labels_meta_points(collapsed_meta_points, 0)
     print("Labeling took {} seconds".format(time.time() - start_time))
 
     n_clusters = len(np.unique(labels_meta_points))
-    confusion_matrix_collapsing = np.zeros((n_clusters, target_clusters+1))
-    confusion_matrix_kmeans = np.zeros((target_clusters+1, target_clusters+1))
+    confusion_matrix_collapsing = np.zeros((n_clusters, 151))
+    confusion_matrix_kmeans = np.zeros((151, 151))
+
     for batch_idx, samples in tqdm(enumerate(dataloader), total=config.num_images):
         if batch_idx == config.num_images:
             break
-        reps = np.asarray(Image.open(png_path_name(config.model, samples[2][0])), dtype=np.uint32)
+        reps = np.asarray(Image.open(os.path.join(METAPOINTS_PATH, samples[2][0].split('.')[0] + '.png')), dtype=np.uint16)
         for i in range(samples[1][0][0].shape[0]):
             for j in range(samples[1][0][0].shape[1]):
                 confusion_matrix_collapsing[labels_meta_points[reps[i*448+j]].flatten(), samples[1][0][0][i,j]] += 1
                 confusion_matrix_kmeans[labels_kmeans[reps[i*448+j]].flatten(), samples[1][0][0][i,j]] += 1
 
-    start_time = time.time()
+    # Compute evaluation metrics
     measurements_collapsing = measure_from_confusion_matrix(confusion_matrix_collapsing)
-    print("Measuring collapsing took {} seconds".format(time.time() - start_time))
-    start_time = time.time()
     measurements_kmeans = measure_from_confusion_matrix(confusion_matrix_kmeans)
-    print("Measuring kmeans took {} seconds".format(time.time() - start_time))
 
+    # Log results to wandb
     wandb.log({
         'assigned_iou_collapsing': measurements_collapsing['assigned_iou'],
         'assigned_miou_collapsing': measurements_collapsing['assigned_miou'],
@@ -198,27 +181,24 @@ def inference(model, dataloader, config=None):
     map_to_label_collapsing = map_pseudo_to_label(measurements_collapsing['assignment'])
     map_to_label_kmeans = map_pseudo_to_label(measurements_kmeans['assignment'])
     mask_list = []
+
     # Send images to WandB
     for batch_idx, samples in enumerate(dataloader):
         if batch_idx == config.num_images:
             break
         # Load image and segmentation
         image = samples[0][0].squeeze().numpy().transpose((1, 2, 0))
-        reps_collapsing = np.asarray(Image.open(png_path_name(config.model, samples[2][0])), dtype=np.uint32)
-
-        # Get segmentation from collapsing
+        reps_collapsing = np.asarray(Image.open(os.path.join(METAPOINTS_PATH, samples[2][0].split('.')[0] + '.png')), dtype=np.uint16)
         segmentation_collapsing = np.array([labels_meta_points[reps_collapsing[i]] for i in range(len(reps_collapsing))]).flatten()
         for i, pseudo in enumerate(segmentation_collapsing.flatten()):
             segmentation_collapsing[i] = map_to_label_collapsing[pseudo] if pseudo in map_to_label_collapsing else 0
         segmentation_collapsing = segmentation_collapsing.reshape((448, 448))
 
-        # Get segmentation from kmeans
         segmentation_kmeans = np.array([labels_kmeans[reps_collapsing[i]] for i in range(len(reps_collapsing))]).flatten()
         for i, pseudo in enumerate(segmentation_kmeans.flatten()):
             segmentation_kmeans[i] = map_to_label_kmeans[pseudo] if pseudo in map_to_label_kmeans else 0
         segmentation_kmeans = segmentation_kmeans.reshape((448, 448))
 
-        # Get true segmentation
         true_segmentation = samples[1][0].squeeze().numpy()
 
         mask_list.append(wandb.Image(image, masks={
@@ -230,6 +210,10 @@ def inference(model, dataloader, config=None):
                 "mask_data": true_segmentation,
                 "class_labels": class_labels
             },
+            # "crf": {
+            #     "mask_data": crf_segmentation,
+            #     "class_labels": class_labels,
+            # },
             "kmeans": {
                 "mask_data": segmentation_kmeans,
                 "class_labels": class_labels,
@@ -237,35 +221,11 @@ def inference(model, dataloader, config=None):
         }))
     wandb.log({"predictions": mask_list})
 
+# wandb.agent(sweep_id, function=inference, count=25)
+
 if __name__ == "__main__":
-    print("Welcome to the evaluation script!")
-    print("The following models are available:")
-    print("1. Segment Anything (SAM).")
-    print("2. DINO.")
-    print("3. DINOv2.")
-    print()
 
-    # Record user input
-    model_num = input("Enter the number of the model you want to use: ")
-    while model_num not in ["1", "2", "3"]:
-        model_num = input("Please enter a valid number: ")
-
-    # Load model
-    if model_num == "1":
-        # SAM
-        model_type = "vit_h"
-        model = sam_model_registry[model_type](checkpoint=CHECKPOINT_PATH).to(DEVICE)
-        model_name = "SAM"
-    elif model_num == "2":
-        # DINO
-        model = torch.hub.load('facebookresearch/dino:main', 'dino_vitb16').to(DEVICE)
-        model_name = "DINO"
-    elif model_num == "3":
-        # DINOv2
-        model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14').to(DEVICE)
-        model_name = "DINOv2"
-
-    # Set hyperparameters
+     # Set hyperparameters
     metric = input("Set the distance metric to be used (cosine or euclidean): ")
     while metric not in ["cosine", "euclidean"]:
         metric = input("Please enter a valid distance metric: ")
@@ -278,22 +238,17 @@ if __name__ == "__main__":
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     num_iters = int(input(f"Select the number of images to process (max {len(dataset)}): "))
-    use_pca = int(input("Do you want to apply PCA? (0 or 1): "))
-    while use_pca not in [0, 1]:
-        use_pca = int(input("Please enter a valid value (0 ir 1): "))
-
-    use_pca = bool(use_pca)
+    while num_iters > len(dataset):
+        num_iters = int(input(f"Please enter a number less than {len(dataset)}: "))
 
     # WandB config
-    wandb.init(project="collapsing")
+    wandb.init(project="collapsing-ade20k")
     wandb.config.update({
         'radius': radius,
         'k_neighbors': k_neighbors,
         'metric': metric,
-        'model': model_name,
         'num_images': num_iters,
-        'use_pca': use_pca
     })
 
-    # Run inference
-    inference(model, dataloader)
+    
+    inference()
